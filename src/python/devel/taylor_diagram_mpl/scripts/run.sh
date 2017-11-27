12a83a69db1582338d74664c953239467a87014b
lee1043 <lee1043@llnl.gov>
2017-11-27 13:30:55 -0800

    just for testing...

---
+++ src/python/devel/taylor_diagram_mpl/scripts/run.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+#python taylor_panel.py -j /work/gleckler1/processed_data/cmip5clims_metrics_package-amip/cmec_11022017/pr_2.5x2.5_regrid2_regrid2_metrics.json -s all -e amip -d global -o test -v pr
+
+#python taylor_panel.py -j /work/gleckler1/processed_data/cmip5clims_metrics_package-amip/cmec_11022017/pr_2.5x2.5_regrid2_regrid2_metrics.json -s all -e amip -d global -o test 
+
+python taylor_panel.py -j /work/gleckler1/processed_data/cmip5clims_metrics_package-amip/cmec_11022017/pr_2.5x2.5_regrid2_regrid2_metrics.json -s all -e amip -d global -o test2 -t uvcdat
